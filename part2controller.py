1da816b530cfcfe194ef7c1a5333d99e4cb38677
osboxes.org <lalocho13@gmail.com>
2020-11-15 16:08:08 -0700

    Working

--- part2controller.py
+++ part2controller.py
@@ -1,8 +1,8 @@
-from pox import *
-#import pox.openflow.libopenflow_01 as of
-#import pox.lib.packet as pkt
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+import pox.lib.packet as pkt
 
-#log = core.getLogger()
+log = core.getLogger()
 
 class Firewall (object):
   """
@@ -10,6 +10,25 @@ class Firewall (object):
   A Connection object for that switch is passed to the __init__ function.
   """
   def __init__ (self, connection):
+    
+
+
+    #Used to create flow rules, no specific ports based on protocol and datalink
+    def addFlowRule(self, nw_proto,dl_type):
+        msg = of.ofp_flow_mod()
+        match = of.ofp_match()
+        # ICMP or ARP opcodes
+        match.nw_proto = nw_proto 
+        #  IP or ARP
+        match.dl_type = dl_type 
+        msg.match = match
+        #L2 L3 Switchting output ports
+        action = of.ofp_action_output(port = of.OFPP_NORMAL)#
+        msg.actions.append(action)
+
+        #sending rule to switch
+        self.connection.send(msg)
+
     # Keep track of the connection to the switch so that we can
     # send it messages!
     self.connection = connection
@@ -18,35 +37,26 @@ class Firewall (object):
     connection.addListeners(self)
 
     #add switch rules here
+    #adding rules for allowing all arp packets
+    addFlowRule(self,pkt.arp.REQUEST,pkt.ethernet.ARP_TYPE)
+    addFlowRule(self,pkt.arp.REPLY,pkt.ethernet.ARP_TYPE)
+
+
+    #adding rule for allowing all icmp packets
     addFlowRule(self,pkt.ipv4.ICMP_PROTOCOL,pkt.ethernet.IP_TYPE)
-    addFLowRUle(self.pkt.arp.REQUEST,pkt.ethernet.ARP_TYPE)
-    addFLowRUle(self.pkt.arp.REPLY,pkt.ethernet.ARP_TYPE)
-    addFLowRUle(self.pkt.arp.REV_REQUEST,pkt.ethernet.ARP_TYPE)
-    addFLowRUle(self.pkt.arp.REV_REPLY,pkt.ethernet.ARP_TYPE)
+
+
+    #adding rule to drop all other packets
     msg = of.ofp_flow_mod()
     match = of.ofp_match()
     msg.match = match
-    msg.hard_timeout = 0
-    msg.soft_timeout = 0
+    #lowest priority
     msg.priorty = 1
+    #No action so everything else is dropped
+    #sending rule to switch
     self.connection.send(msg)
 
-  def addFlowRule(self, nw_proto,dl_type):
-        msg = of.ofp_flow_mod()
-        match = of.ofp_match()
-        match.nw_src = None
-        match.nw_dst = None
-        match.tp_src = None
-        match.tp_dst = None
-        match.nw_proto = nw_proto # 1 for ICMP or ARP opcode
-        match.dl_type = dl_type # == 0x0800 for IP, 0x0806 for ARP
-        msg.match = match
-        msg.hard_timeout = 0
-        msg.soft_timeout = 0
-        msg.priority = 32768
-        action = of.ofp_action_output(port = of.OFPP_NORMAL)
-        msg.actions.append(action)
-        self.connection.send(msg)
+  
     
   def _handle_PacketIn (self, event):
     """
@@ -59,13 +69,7 @@ class Firewall (object):
     if not packet.parsed:
       log.warning("Ignoring incomplete packet")
       return
-    """if packet.type == packet.ARP_TYPE:
-    elif packet = packet.IP_TYPE:
-        ippacket  = packet.payload
-        if ippacket.protocol == packet.ICMP_PROTOCOL:
-            icmpPacket = ippacket.payload
-    packet_in = event.ofp # The actual ofp_packet_in message.
-    print ("Unhandled packet :" + str(packet.dump()))"""
+   
 
 def launch ():
   """
